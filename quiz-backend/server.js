4065f4bb19490924e2c11ef9e1992355a690d2e6
Liandrolima <156594619+Liandrolima@users.noreply.github.com>
2024-10-21 17:14:34 -0300

    cors

--- quiz-backend/server.js
+++ quiz-backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+app.use(cors());
 const { Pool } = require('pg');
 
 const app = express();
