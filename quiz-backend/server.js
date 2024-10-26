be4d6b0df887433cd44d602488d1eaee57ecc8fd
Liandrolima <156594619+Liandrolima@users.noreply.github.com>
2024-10-25 22:34:44 -0300

    Update server.js

--- quiz-backend/server.js
+++ quiz-backend/server.js
@@ -1,34 +1,36 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
+    const express = require('express');
+    const cors = require('cors');
+    const app = express();
 
-// Configurar CORS
-app.use(cors({
-    origin: 'https://liandrolima.github.io', // O domínio do frontend
-    methods: ['GET', 'POST', 'OPTIONS'], // Métodos permitidos
-    allowedHeaders: ['Content-Type', 'Authorization', 'apikey'], // Cabeçalhos permitidos
-    credentials: true
-}));
+    // Configurar CORS
+    app.use(cors({
+        origin: 'https://liandrolima.github.io', // O domínio do frontend
+        methods: ['GET', 'POST', 'OPTIONS'], // Métodos permitidos
+        allowedHeaders: ['Content-Type', 'Authorization', 'apikey'], // Cabeçalhos permitidos
+        credentials: true
+    }));
 
-// Outras rotas e middlewares
-app.use(express.json());
+    // Outras rotas e middlewares
+    app.use(express.json());
 
-app.options('/resultados', (req, res) => {
-    console.log('Recebida requisição preflight para /resultados');
-    res.header('Access-Control-Allow-Origin', 'https://liandrolima.github.io');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, apikey');
-    res.sendStatus(204); // Enviar status 204 para confirmar o sucesso da requisição preflight
-});
+    app.options('/resultados', (req, res) => {
+        res.header('Access-Control-Allow-Origin', 'https://liandrolima.github.io');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, apikey');
+        res.sendStatus(204);        
+    });
 
-app.post('/resultados', (req, res) => {
-    const { nome, acertos, total } = req.body;
-    console.log(`Nome: ${nome}, Acertos: ${acertos}, Total: ${total}`);
-    res.status(200).json({ message: 'Dados recebidos com sucesso' });
-});
+    app.post('/resultados', (req, res) => {
+        const { nome, acertos, total } = req.body;
+        if (!nome || typeof acertos !== 'number' || !Array.isArray(total)) {
+            return res.status(400).json({ message: 'Dados inválidos' });
+        }
+        console.log(`Nome: ${nome}, Acertos: ${acertos}, Total: ${total}`);
+        res.status(200).json({ message: 'Dados recebidos com sucesso' });
+    });
 
-// Configurar a porta
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
-});
+    // Configurar a porta
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`Servidor rodando na porta ${port}`);
+    });
