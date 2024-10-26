7a23b370ea9a5fda1abe966b3dc0b5f563170bc8
Liandrolima <156594619+Liandrolima@users.noreply.github.com>
2024-10-25 21:00:46 -0300

    ok

--- quiz-backend/server.js
+++ quiz-backend/server.js
@@ -2,15 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// Configurar CORS para permitir o cabeçalho 'apikey'
 app.use(cors({
-    origin: 'https://liandrolima.github.io', // Domínio do frontend
+    origin: 'https://liandrolima.github.io',
     methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'apikey'], // Inclui 'apikey'
+    allowedHeaders: ['Content-Type', 'Authorization', 'apikey'], // Inclui 'apikey' explicitamente
     credentials: true
 }));
 
-// Configurações de outras rotas e middlewares
 app.use(express.json());
 
 app.post('/resultados', (req, res) => {
@@ -19,7 +17,6 @@ app.post('/resultados', (req, res) => {
     res.status(200).json({ message: 'Dados recebidos com sucesso' });
 });
 
-// Configurar a porta
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
