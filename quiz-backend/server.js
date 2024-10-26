cdd2eba56b102092affc2f9d2b326cdcf841551b
Liandrolima <156594619+Liandrolima@users.noreply.github.com>
2024-10-25 21:44:56 -0300

    Update server.js

--- quiz-backend/server.js
+++ quiz-backend/server.js
@@ -2,26 +2,24 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// Configurar CORS para permitir o cabeçalho 'apikey'
+// Configurar CORS
 app.use(cors({
-    origin: 'https://liandrolima.github.io', // Domínio do frontend
+    origin: 'https://liandrolima.github.io', // O domínio do frontend
     methods: ['GET', 'POST', 'OPTIONS'], // Métodos permitidos
     allowedHeaders: ['Content-Type', 'Authorization', 'apikey'], // Cabeçalhos permitidos
     credentials: true
 }));
 
-// Monitorar e responder explicitamente à requisição preflight (OPTIONS) para o endpoint /resultados
+// Outras rotas e middlewares
+app.use(express.json());
+
 app.options('/resultados', (req, res) => {
-    res.set({
-        'Access-Control-Allow-Origin': 'https://liandrolima.github.io',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization, apikey'
-    });
+    console.log('Recebida requisição preflight para /resultados');
+    res.header('Access-Control-Allow-Origin', 'https://liandrolima.github.io');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, apikey');
     res.sendStatus(204); // Enviar status 204 para confirmar o sucesso da requisição preflight
 });
-
-// Outras configurações do servidor
-app.use(express.json());
 
 app.post('/resultados', (req, res) => {
     const { nome, acertos, total } = req.body;
@@ -29,7 +27,7 @@ app.post('/resultados', (req, res) => {
     res.status(200).json({ message: 'Dados recebidos com sucesso' });
 });
 
-// Configuração da porta
+// Configurar a porta
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
